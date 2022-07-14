bfa9a497943a78ddd0cfe58e97f126a643cfd84b
Sourav2707 <smartsourav33@gmail.com>
2022-07-14 12:35:47 +0530

    Initial commit

--- EmployeeWage/EmployeeWage/Program.cs
+++ EmployeeWage/EmployeeWage/Program.cs
@@ -1,4 +1,1 @@
-﻿using EmployeeWage;
-Console.WriteLine("Welcome to employee wage computation program");
-Emp emp = new Emp(); //emp as object
-emp.Wage();
+﻿Console.WriteLine("Welcome to employee wage computation program");
